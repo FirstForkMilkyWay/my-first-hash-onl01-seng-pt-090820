a3777f497d69d373bff2f13bae35eb95c02e7354
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-17 18:15:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 your_hash = {"band" => "megadeth}"
-  
+end
 end
 
 
@@ -43,4 +43,5 @@ def adding
 
   # return the shipping_manifest hash below
 return shipping_manifest
+end
 end
